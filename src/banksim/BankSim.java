7248fa787d0f435acd16a0dcdbf4c9a41461822c
hildreta <hildreth.alex@gmail.com>
2016-06-26 20:10:30 -0400

    added further basic functionality

--- src/banksim/BankSim.java
+++ src/banksim/BankSim.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package banksim;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *
@@ -20,6 +20,7 @@ public class BankSim {
         Scanner in = new Scanner(System.in);
         int selection;
         boolean running = true;
+        HashMap accountList = new HashMap();
         
         while(running){
             System.out.println("\n**********************************");
@@ -31,10 +32,62 @@ public class BankSim {
             selection = in.nextInt();
             
             //process selection
-            if(selection == 3){
+            if(selection == 1){
+                makeAcct(accountList);
+            }
+            else if(selection == 2){
+                accessAcct(accountList);
+            }
+            else if(selection == 3){
                 running = false;
             }
         }
     }
     
+    
+    //account creation function
+    public static void makeAcct(HashMap accountList){
+        Scanner in = new Scanner(System.in);
+        String newName;
+        double newBal;
+        
+        System.out.println("\n**********************************");
+        System.out.println("Enter New Account Name: ");
+        newName = in.nextLine();
+        System.out.println("Enter initial balance: ");
+        newBal = in.nextDouble();
+        
+        accountList.put(newName, new SavingsAccount(newName, newBal));
+        
+        System.out.println("Creation successful, returning to menu...");
+    }
+    
+    
+    //access and edit account function
+    public static void accessAcct(HashMap accountList){
+        Scanner in = new Scanner(System.in);
+        String acctName;
+        int selection;
+        
+        System.out.println("\n**********************************");
+        System.out.println("Enter account name: ");
+        acctName = in.nextLine();
+        
+        System.out.println("1. Print Account Info");
+      
+        selection = in.nextInt();
+        
+        if(selection == 1){
+            printInfo(acctName, accountList);
+        }
+        
+    }
+    
+    public static void printInfo(String name, HashMap accountList){
+        System.out.println("\n**********************************");
+        System.out.printf("Name: %s\n", name);
+        System.out.printf("Balance: ");
+        System.out.println(accountList.get(name).getSavBal());
+        
+    }
 }
